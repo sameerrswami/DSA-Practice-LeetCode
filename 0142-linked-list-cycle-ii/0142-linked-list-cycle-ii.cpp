046bce6dc77be43d9e5c315e5103593c7be972b9
Sameer Swami <147032027+sameerrswami@users.noreply.github.com>
2026-07-26 17:51:06 +0530

    Time: 5 ms (83.26%), Space: 13.9 MB (7.27%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
         ListNode* t=head;
+        // simple sa code hai... bas agar wo pahle se visited hai to wo return kardo
         unordered_map<ListNode*,bool> mp;
         while(t && t->next){
             if(mp.count(t))return t;
