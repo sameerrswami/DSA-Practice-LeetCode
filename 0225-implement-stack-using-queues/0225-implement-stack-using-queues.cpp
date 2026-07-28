b2db33a2662abeb104f71659ff90df57c02a2b02
Sameer Swami <147032027+sameerrswami@users.noreply.github.com>
2026-07-29 02:09:52 +0530

    Time: 0 ms (100%), Space: 10.1 MB (41.97%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -0,0 +1,27 @@
+class MyStack {
+    queue<int> q;
+public:
+    MyStack() {}
+    void push(int x) {
+        q.push(x);
+        int s=q.size();
+        while(--s){
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        int x=q.front();
+        q.pop();
+        return x;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return (q.empty());
+    }
+};
