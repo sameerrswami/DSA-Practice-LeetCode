a2bab410bb0add8463b7b88b88f4da4c2491ec2c
Sameer Swami <147032027+sameerrswami@users.noreply.github.com>
2026-08-02 14:38:18 +0530

    Time: 35 ms (90.99%), Space: 62.6 MB (73.63%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    void subsets(vector<int> &v,vector<vector<int>> &ans,vector<int> &temp,int i,int k){
+    void subsets(int n,int k,vector<vector<int>> &ans,vector<int> &temp,int i){
         if(temp.size()==k){
             ans.push_back(temp);
             return;
         }
-        if(i==v.size())return;
-        temp.push_back(v[i]);
-        subsets(v,ans,temp,i+1,k);
-        temp.pop_back();
-        subsets(v,ans,temp,i+1,k);
+        for(int ind=i;ind<=n;ind++){
+            temp.push_back(ind);
+            subsets(n,k,ans,temp,ind+1);
+            temp.pop_back();
+        }
     }
     vector<vector<int>> combine(int n, int k) {
-        vector<int> v;
-        for(int i=1;i<=n;i++)v.push_back(i);
         vector<vector<int>> ans;
         vector<int> temp;
-        subsets(v,ans,temp,0,k);
+        subsets(n,k,ans,temp,1);
         return ans;
     }
 };
