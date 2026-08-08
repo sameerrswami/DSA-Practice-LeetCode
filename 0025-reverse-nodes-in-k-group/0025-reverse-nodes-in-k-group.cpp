41aac7fec89b26ce0b6b0741052f9df135303422
Sameer Swami <147032027+sameerrswami@users.noreply.github.com>
2026-08-08 11:01:29 +0530

    Time: 0 ms (100%), Space: 16.4 MB (97.16%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -10,24 +10,42 @@ public:
         }
         return prev;
     }
-    ListNode* reverseKGroup(ListNode* head, int k) {
-        ListNode dummy(0);
-        dummy.next = head;
-        ListNode* prevGroup = &dummy;
-        while (true) {
-            ListNode* kth = prevGroup;
-            for (int i = 0; i < k && kth; i++)
-                kth = kth->next;
-            if (!kth)
-                break;
-            ListNode* nextGroup = kth->next;
-            ListNode* groupHead = prevGroup->next;
-            kth->next = nullptr;
-            ListNode* newHead = reverse(groupHead);
-            prevGroup->next = newHead;
-            groupHead->next = nextGroup;
-            prevGroup = groupHead;
+
+    ListNode* reverseKGroup(ListNode* head , int k){
+        ListNode* temp=head;
+        int cnt=1;
+        while (cnt < k && temp != nullptr) {
+            temp = temp->next;
+            cnt++;
         }
-        return dummy.next;
+        if (temp == nullptr) {
+            return head;
+        }
+        ListNode* nextGroup = temp->next;
+        temp->next = nullptr;
+        ListNode* newHead = reverse(head);
+        head->next = reverseKGroup(nextGroup, k);
+        return newHead;
     }
+
+    // ListNode* reverseKGroup(ListNode* head, int k) {
+    //     ListNode dummy(0);
+    //     dummy.next = head;
+    //     ListNode* prevGroup = &dummy;
+    //     while (true) {
+    //         ListNode* kth = prevGroup;
+    //         for (int i = 0; i < k && kth; i++)
+    //             kth = kth->next;
+    //         if (!kth)
+    //             break;
+    //         ListNode* nextGroup = kth->next;
+    //         ListNode* groupHead = prevGroup->next;
+    //         kth->next = nullptr;
+    //         ListNode* newHead = reverse(groupHead);
+    //         prevGroup->next = newHead;
+    //         groupHead->next = nextGroup;
+    //         prevGroup = groupHead;
+    //     }
+    //     return dummy.next;
+    // }
 };
