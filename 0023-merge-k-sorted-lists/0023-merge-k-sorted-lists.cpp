41d7074d650c1e72fa46f614ed69ddaddca2ecbe
Sameer Swami <147032027+sameerrswami@users.noreply.github.com>
2026-08-08 19:00:15 +0530

    Time: 3 ms (61.18%), Space: 18.5 MB (66.16%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -1,41 +1,39 @@
 class Solution {
 public:
-    ListNode* merge(ListNode* l1,ListNode* l2){
-        if(!l1) return l2;
-        if(!l2) return l1;
-        ListNode* t1=l1, *t2=l2;
-        ListNode* dummy=new ListNode(0);
-        ListNode*t = dummy;
-        while(t1 && t2){
-            if(t1->val<t2->val){
-                t->next=t1;
-                t=t->next;
-                t1=t1->next;
-            }
-            else{
-                t->next=t2;
-                t=t->next;
-                t2=t2->next;
-            }
+
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+        if (!l1) return l2;
+        if (!l2) return l1;
+
+        if (l1->val < l2->val) {
+            l1->next = merge(l1->next, l2);
+            return l1;
         }
-        while(t1){
-            t->next=t1;
-            t=t->next;
-            t1=t1->next;
+        else {
+            l2->next = merge(l1, l2->next);
+            return l2;
         }
-        while(t2){
-            t->next=t2;
-            t=t->next;
-            t2=t2->next;
-        }
-        return dummy->next;
     }
+
+    ListNode* solve(vector<ListNode*>& lists, int l, int r) {
+        if (l > r)
+            return nullptr;
+
+        if (l == r)
+            return lists[l];
+
+        int mid = l + (r - l) / 2;
+
+        ListNode* left = solve(lists, l, mid);
+        ListNode* right = solve(lists, mid + 1, r);
+
+        return merge(left, right);
+    }
+
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if(lists.size()==1)return lists[0];
-        ListNode* ans = nullptr;
-        for(int i=0;i<lists.size();i++){
-            ans=merge(ans,lists[i]);
-        }
-        return ans;
+        if (lists.empty())
+            return nullptr;
+
+        return solve(lists, 0, lists.size() - 1);
     }
 };
