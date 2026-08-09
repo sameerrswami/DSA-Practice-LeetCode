4a11ceb42d3fb0b983f98069065510facbb0bf2c
Sameer Swami <147032027+sameerrswami@users.noreply.github.com>
2026-08-09 21:33:15 +0530

    Time: 7 ms (35.67%), Space: 28.4 MB (64.99%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -5,12 +5,8 @@ public:
         vector<int> res(nums.size(),-1);
         for(int i=nums.size()*2-1;i>=0;i--){
             int j=i%nums.size();
-            while(!st.empty() && st.top()<=nums[j]){
-                st.pop();
-            }
-            if(!st.empty()){
-                res[j]=st.top();
-            }
+            while(!st.empty() && st.top()<=nums[j]) st.pop();
+            if(!st.empty()) res[j]=st.top();
             st.push(nums[j]);
         }
         return res;
