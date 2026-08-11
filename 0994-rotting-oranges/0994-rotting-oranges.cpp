b2d4b9f3b2b4a336cd96e5873923085b184c9a1f
Sameer Swami <147032027+sameerrswami@users.noreply.github.com>
2026-08-11 20:20:24 +0530

    Time: 0 ms (100%), Space: 16.6 MB (97.86%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -14,33 +14,51 @@ public:
             }
         }
         if(fc==0)return 0; // if no fresh then no bfs
+        // we using TDLR checking
+        int row[4]={-1,1,0,0};
+        int col[4]={0,0,-1,1};
         while(!q.empty()){ // start bfs 
             int size=q.size();
             while(size--){ // now we used this as we want to perform bfs on all the index presed inside it..
-                int row=q.front().first;
-                int col=q.front().second;
+                int r=q.front().first;
+                int c=q.front().second;
                 q.pop();
-                //checking all 4 directions
-                if(col+1<n && grid[row][col+1]==1){  // right
-                    grid[row][col+1]=2; 
-                    q.push({row,col+1});  // pushed the freshly rotten oranges 
-                    fc--;
-                } 
-                if(col-1>=0 &&grid[row][col-1]==1){ // left
-                    grid[row][col-1]=2;
-                    q.push({row,col-1});
-                    fc--;
+
+                //using row and coll arrays and a loop
+
+                for(int i=0;i<4;i++){
+                    int nr=row[i]+r;
+                    int nc=col[i]+c;
+                    if(nc<0 || nr<0 || nr>=m || nc>=n) continue;
+                    if(grid[nr][nc]==1){
+                        grid[nr][nc]=2;
+                        q.push({nr,nc});
+                        fc--;
+                    }
                 }
-                if(row+1<m && grid[row+1][col]==1){ // down
-                    grid[row+1][col]=2;
-                    q.push({row+1,col});
-                    fc--;
-                }
-                if(row-1>=0 && grid[row-1][col]==1){ // up
-                    grid[row-1][col]=2;
-                    q.push({row-1,col});
-                    fc--;
-                }
+
+                // //checking all 4 directions
+                // if(col+1<n && grid[row][col+1]==1){  // right
+                //     grid[row][col+1]=2; 
+                //     q.push({row,col+1});  // pushed the freshly rotten oranges 
+                //     fc--;
+                // } 
+                // if(col-1>=0 &&grid[row][col-1]==1){ // left
+                //     grid[row][col-1]=2;
+                //     q.push({row,col-1});
+                //     fc--;
+                // }
+                // if(row+1<m && grid[row+1][col]==1){ // down
+                //     grid[row+1][col]=2;
+                //     q.push({row+1,col});
+                //     fc--;
+                // }
+                // if(row-1>=0 && grid[row-1][col]==1){ // up
+                //     grid[row-1][col]=2;
+                //     q.push({row-1,col});
+                //     fc--;
+                // }
+
             }
             tt++; // one minute added after every loop
             if(fc==0)return tt; // if no fresh orange then return total time
