e7581a3bf90af143433d45d6a3ed5283bc09347e
Sameer Swami <147032027+sameerrswami@users.noreply.github.com>
2026-08-13 15:58:02 +0530

    Time: 16 ms (6.93%), Space: 23.9 MB (56.62%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int r=0,l=0;
+        while(r<nums.size()){
+            if(nums[r]!=0){
+                swap(nums[l++],nums[r]);
+            }
+            r++;
+        }
+    }
+};
