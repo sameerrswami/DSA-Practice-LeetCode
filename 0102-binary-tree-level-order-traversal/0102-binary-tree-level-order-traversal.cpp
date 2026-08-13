ab040012133213a0e6115f58d262d964b3dc7d70
Sameer Swami <147032027+sameerrswami@users.noreply.github.com>
2026-08-13 19:50:41 +0530

    Time: 0 ms (100%), Space: 17.3 MB (5.36%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -5,17 +5,18 @@ public:
         if(!root)return res;
         queue<TreeNode*> q;
         q.push(root);
+        int i=0;
         while(!q.empty()){
             int s=q.size();
-            vector<int> v;
+            res.push_back({});
             while(s--){
                 TreeNode* node=q.front();
                 q.pop();
-                v.push_back(node->val);
+                res[i].push_back(node->val);
                 if(node->left) q.push(node->left);
                 if(node->right) q.push(node->right);
             }
-            res.push_back(v);
+            i++;
         }
         return res;
     }
