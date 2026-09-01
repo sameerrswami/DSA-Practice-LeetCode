8fa08c4604951114b1db57959a4fb93417e24230
Sameer Swami <147032027+sameerrswami@users.noreply.github.com>
2026-09-01 22:31:55 +0530

    Time: 0 ms (100%), Space: 35.7 MB (55.49%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int ts=accumulate(nums.begin(),nums.end(),0);
+        int cs=0;
+        for(int i=0;i<nums.size();i++){
+            ts=ts-nums[i];
+            if(cs==ts) return i;
+            cs+=nums[i];
+        }
+        return -1;
+    }
+};
