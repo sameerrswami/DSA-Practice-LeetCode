70cad7c565e2a5f086f97bc6f1332713556d1616
Sameer Swami <147032027+sameerrswami@users.noreply.github.com>
2026-09-02 19:31:23 +0530

    Time: 0 ms (100%), Space: 10.2 MB (97.3%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -3,8 +3,7 @@ public:
     void subsets(vector<int>&nums,vector<vector<int>> &ans,int ind,vector<int> &temp){
         ans.push_back(temp);
         for(int i = ind; i < nums.size(); i++) {
-            if(i > ind && nums[i] == nums[i - 1])
-                continue;
+            if(i > ind && nums[i] == nums[i - 1]) continue;
             temp.push_back(nums[i]);
             subsets(nums,ans,i+1,temp);
             temp.pop_back();
