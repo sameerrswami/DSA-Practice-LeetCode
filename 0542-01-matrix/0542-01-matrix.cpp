2f5b313ab53295a464b9c540818a76bc198c68b5
Sameer Swami <147032027+sameerrswami@users.noreply.github.com>
2026-09-02 15:18:38 +0530

    Time: 9 ms (88.67%), Space: 34.5 MB (93.48%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        queue<pair<int,int>> q;
-        int m=mat.size(),n=mat[0].size();
-        for(int  i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(mat[i][j]==0) q.push({i,j});
-                else mat[i][j]=-1;
+        queue<pair<int, int>> q;
+        int m = mat.size(), n = mat[0].size();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0)
+                    q.push({i, j});
+                else
+                    mat[i][j] = -1;
             }
         }
-        int  d=1;
-        while(!q.empty()){
-            int size=q.size();
-            while(size--){
-                int i=q.front().first;
-                int j=q.front().second;
-                q.pop();
-                int row[4]={-1,1,0,0};
-                int col[4]={0,0,-1,1};
-                for(int k=0;k<4;k++){
-                    int ni=i+row[k];
-                    int nj=j+col[k];
-                    if(ni<0 || nj<0 || ni>=m || nj>=n) continue;
-                    if(mat[ni][nj]==-1){
-                        mat[ni][nj]=d;
-                        q.push({ni,nj});
-                    }
+        while (!q.empty()) {
+            auto [i, j] = q.front();
+            q.pop();
+            int row[4] = {-1, 1, 0, 0};
+            int col[4] = {0, 0, -1, 1};
+            for (int k = 0; k < 4; k++) {
+                int ni = i + row[k];
+                int nj = j + col[k];
+                if (ni >= 0 && nj >= 0 && ni < m && nj < n && mat[ni][nj] == -1)
+                {
+                    mat[ni][nj] = mat[i][j] + 1;
+                    q.push({ni, nj});
                 }
             }
-            d++;
         }
-        return mat;
+    return mat;
     }
 };
