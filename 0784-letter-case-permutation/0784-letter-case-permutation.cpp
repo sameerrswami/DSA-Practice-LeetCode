62da9c4d99314d6e38028135475286c29e169183
Sameer Swami <147032027+sameerrswami@users.noreply.github.com>
2026-09-04 22:09:09 +0530

    Time: 3 ms (53.54%), Space: 13.2 MB (71.69%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.cpp
+++ 0784-letter-case-permutation/0784-letter-case-permutation.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     vector<string> res;
-    void perms(string &s, int i) {
-        if (i == s.size()) {
+    void perms(string &s,int i){
+        if(i==s.size()){
             res.push_back(s);
             return;
         }
-        if (isdigit(s[i])) {
-            perms(s, i + 1);
-            return;
+        if(isalpha(s[i])){
+            s[i]=tolower(s[i]);
+            perms(s,i+1);
+            s[i]=toupper(s[i]);
+            perms(s,i+1);
         }
-        s[i] = tolower(s[i]);
-        perms(s, i + 1);
-        s[i] = toupper(s[i]);
-        perms(s, i + 1);
+        else{
+            perms(s,i+1);
+        }
     }
     vector<string> letterCasePermutation(string s) {
-        perms(s, 0);
+        perms(s,0);
         return res;
     }
 };
