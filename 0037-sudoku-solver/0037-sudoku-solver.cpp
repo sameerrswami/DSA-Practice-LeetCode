6ce07c4ab6a6d7ad342d9d0c810097201a13ab92
Sameer Swami <147032027+sameerrswami@users.noreply.github.com>
2026-09-06 19:38:33 +0530

    Time: 55 ms (96.39%), Space: 9 MB (6.66%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
-    bool find(vector<vector<char>>& board,int i,int j){
-        if(i==9) return 1;
-        if(j==9) return find(board,i+1,0);
-        if(board[i][j]!='.') return find(board,i,j+1);
-        for(int num=1;num<10;num++){
-            if(check(board,i,j,num)){
-                board[i][j]='0'+num;
-                if(find(board,i,j+1)) return 1;
-                board[i][j]='.';
+    bool row[9][10] = {}, col[9][10] = {}, box[9][10] = {};
+
+    bool solve(vector<vector<char>>& board, int i, int j) {
+        if(i == 9) return true;
+        if(j == 9) return solve(board, i + 1, 0);
+        if(board[i][j] != '.') return solve(board, i, j + 1);
+
+        int b = (i / 3) * 3 + j / 3;
+
+        for(int num = 1; num <= 9; num++) {
+            if(!row[i][num] && !col[j][num] && !box[b][num]) {
+                board[i][j] = num + '0';
+                row[i][num] = col[j][num] = box[b][num] = true;
+
+                if(solve(board, i, j + 1)) return true;
+
+                board[i][j] = '.';
+                row[i][num] = col[j][num] = box[b][num] = false;
             }
         }
-        return 0;
+        return false;
     }
-    bool check(vector<vector<char>>& board , int i,int j,int num){
-        char c = '0'+num;
-        for(int row=0;row<9;row++){
-            if(board[row][j]==c) return 0;
-        }
-        for(int col=0;col<9;col++){
-            if(board[i][col]==c) return 0;
-        }
-        int row=(i/3)*3,col=(j/3)*3;
-        for(int a=row;a<row+3;a++){
-            for(int b=col;b<col+3;b++){
-                if(board[a][b]==c) return 0;
-            }
-        }
-        return 1;
-    }
+
     void solveSudoku(vector<vector<char>>& board) {
-        find(board,0,0);
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j] != '.'){
+                    int num = board[i][j] - '0';
+                    int b = (i/3)*3 + j/3;
+                    row[i][num] = col[j][num] = box[b][num] = true;
+                }
+            }
+        }
+        solve(board,0,0);
     }
 };
