cd52aa4e7fe8c596765adcb15ae736cafa80833b
Sameer Swami <147032027+sameerrswami@users.noreply.github.com>
2026-09-06 18:35:58 +0530

    Time: 2 ms (72.83%), Space: 10.2 MB (77.68%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,30 +1,18 @@
 class Solution {
 public:
-
-    bool check(int n,vector<string>&Board,int i,int j){
-        int row=i,col=j;
-        // upper left diagnol
-        while(row>-1 && col>-1){
-            if(Board[row][col]=='Q') return 0;
-            row--,col--;
-        }
-        row=i,col=j;
-        // upper right diagnol
-        while(row>-1 && col<n){
-            if(Board[row][col]=='Q') return 0;
-            row--,col++;
-        }
-        return 1;
-    }
-    void find(int row,int n,vector<vector<string>> &ans,vector<string> &Board,vector<bool>& column){
+    void find(int row,int n,vector<vector<string>> &ans,vector<string> &Board,vector<bool>& column,vector<bool> &leftDg,vector<bool> &rightDg){
         if(row==n){
             ans.push_back(Board);
         }
         for(int j=0;j<n;j++){
-            if(column[j]==0 && check(n,Board,row,j)){
+            if(column[j]==0 && leftDg[row-j+n-1]==0 && rightDg[row+j]==0){
+                rightDg[row+j]=1;
+                leftDg[(row-j)+(n-1)]=1;
                 column[j]=1;
                 Board[row][j]='Q';
-                find(row+1,n,ans,Board,column);
+                find(row+1,n,ans,Board,column,leftDg,rightDg);
+                rightDg[row+j]=0;
+                leftDg[(row-j)+(n-1)]=0;
                 column[j]=0;
                 Board[row][j]='.';
             }
@@ -33,13 +21,15 @@ public:
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
         vector<string>Board(n);
+        vector<bool> rightDg((n*2)-1,0);
+        vector<bool> leftDg((n*2)-1,0);
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 Board[i].push_back('.');
             }
         }
         vector<bool> column(n,0);
-        find(0,n,ans,Board,column);
+        find(0,n,ans,Board,column,leftDg,rightDg);
         return ans;
     }
 };
