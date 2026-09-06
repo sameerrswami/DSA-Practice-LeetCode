3fcc4764c2b7d976a9a2e89b03074accf0fc48b6
Sameer Swami <147032027+sameerrswami@users.noreply.github.com>
2026-09-06 18:13:14 +0530

    Time: 3 ms (61.18%), Space: 10.2 MB (58.33%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+
+    bool check(int n,vector<string>&Board,int i,int j){
+        int row=i,col=j;
+        // upper left diagnol
+        while(row>-1 && col>-1){
+            if(Board[row][col]=='Q') return 0;
+            row--,col--;
+        }
+        row=i,col=j;
+        // upper right diagnol
+        while(row>-1 && col<n){
+            if(Board[row][col]=='Q') return 0;
+            row--,col++;
+        }
+        return 1;
+    }
+    void find(int row,int n,vector<vector<string>> &ans,vector<string> &Board,vector<bool>& column){
+        if(row==n){
+            ans.push_back(Board);
+        }
+        for(int j=0;j<n;j++){
+            if(column[j]==0 && check(n,Board,row,j)){
+                column[j]=1;
+                Board[row][j]='Q';
+                find(row+1,n,ans,Board,column);
+                column[j]=0;
+                Board[row][j]='.';
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string>Board(n);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                Board[i].push_back('.');
+            }
+        }
+        vector<bool> column(n,0);
+        find(0,n,ans,Board,column);
+        return ans;
+    }
+};
