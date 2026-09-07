afab2a7772bf96559e91992c00ebbbd6a2612bb2
Sameer Swami <147032027+sameerrswami@users.noreply.github.com>
2026-09-07 20:01:25 +0530

    Time: 0 ms (100%), Space: 16.5 MB (97.55%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,68 +1,37 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        // used Multi source BFS in this means our bfs will start from multiple indexes at the same time
-        int m=grid.size();
-        int n=grid[0].size();
-        int tt=0; //total time
-        int fc=0; //fresh count
-        queue<pair<int,int>> q;  //queue to store rotten oramges
+        int m=grid.size(),n=grid[0].size();
+        int fc=0;
+        queue<pair<int,int>> q;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]==2) q.push({i,j}); //stored all rotten oranges as bfs will start from all oranges
-                if(grid[i][j]==1) fc++; // fresh count track as we have to stop if all are rotten
+                if(grid[i][j]==1) fc++;
+                if(grid[i][j]==2) q.push({i,j});
             }
         }
-        if(fc==0)return 0; // if no fresh then no bfs
-        // we using TDLR checking
-        int row[4]={-1,1,0,0};
-        int col[4]={0,0,-1,1};
-        while(!q.empty()){ // start bfs 
-            int size=q.size();
-            while(size--){ // now we used this as we want to perform bfs on all the index presed inside it..
-                int r=q.front().first;
-                int c=q.front().second;
+        int row[4]={1,-1,0,0};
+        int col[4]={0,0,1,-1};
+        int c=0;
+        if(fc==0)return 0;
+        while(!q.empty()){
+            int size=q.size();            
+            while(size--){
+                auto[i,j]=q.front();
                 q.pop();
-
-                //using row and coll arrays and a loop
-
-                for(int i=0;i<4;i++){
-                    int nr=row[i]+r;
-                    int nc=col[i]+c;
-                    if(nc<0 || nr<0 || nr>=m || nc>=n) continue;
-                    if(grid[nr][nc]==1){
-                        grid[nr][nc]=2;
-                        q.push({nr,nc});
+                for(int k=0;k<4;k++){
+                    int ni=i+row[k];
+                    int nj=j+col[k];
+                    if(ni>=0 && nj>=0 && ni<m && nj<n && grid[ni][nj]==1){
+                        grid[ni][nj]=2;
+                        q.push({ni,nj});
                         fc--;
                     }
                 }
-
-                // //checking all 4 directions
-                // if(col+1<n && grid[row][col+1]==1){  // right
-                //     grid[row][col+1]=2; 
-                //     q.push({row,col+1});  // pushed the freshly rotten oranges 
-                //     fc--;
-                // } 
-                // if(col-1>=0 &&grid[row][col-1]==1){ // left
-                //     grid[row][col-1]=2;
-                //     q.push({row,col-1});
-                //     fc--;
-                // }
-                // if(row+1<m && grid[row+1][col]==1){ // down
-                //     grid[row+1][col]=2;
-                //     q.push({row+1,col});
-                //     fc--;
-                // }
-                // if(row-1>=0 && grid[row-1][col]==1){ // up
-                //     grid[row-1][col]=2;
-                //     q.push({row-1,col});
-                //     fc--;
-                // }
-
             }
-            tt++; // one minute added after every loop
-            if(fc==0)return tt; // if no fresh orange then return total time
+            c++;
+            if(fc==0) return c;
         }
-        return -1; // if there was fresh remaining means the upper condtion didnt run so we have returned -1...
+        return -1;
     }
 };
