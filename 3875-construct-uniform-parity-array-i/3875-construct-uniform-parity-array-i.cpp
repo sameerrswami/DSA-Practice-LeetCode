07bd554b640dab4ee960ad182179087363cddfcd
Sameer Swami <147032027+sameerrswami@users.noreply.github.com>
2026-09-08 14:39:05 +0530

    Time: 0 ms (100%), Space: 30.3 MB (30.11%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        bool isOdd=true,isEven=true;
-        for(int i:nums1){
-            if(i%2==0) isOdd=false;
-            if(i%2!=0) isEven=false;
-        }
-        if(isOdd || isEven || !isEven)return true;
-        return  false;
+       return true;
     }
 };
