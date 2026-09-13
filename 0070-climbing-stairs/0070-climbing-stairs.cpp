95efba69ef0b8f4a33dd4ae41bf87ece109899a2
Sameer Swami <147032027+sameerrswami@users.noreply.github.com>
2026-09-13 23:20:44 +0530

    Time: 0 ms (100%), Space: 8.5 MB (53.72%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int solve(vector<int>& dp,int n){
-        if(n==1)return 1;
-        if(n==2)return 2;
+        if(n<=2)return n;
         if(dp[n]!=-1)return dp[n];
         return dp[n]=solve(dp,n-1)+solve(dp,n-2);
     }
