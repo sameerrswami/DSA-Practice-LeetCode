e087e28aac55d01239baca7b5a370530dfafa811
Sameer Swami <147032027+sameerrswami@users.noreply.github.com>
2026-09-22 16:49:35 +0530

    Time: 0 ms (100%), Space: 22.7 MB (52.01%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -3,11 +3,8 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int l=0,r=1;
         while(r<nums.size()){
-            if(nums[l]==nums[r]){
-                r++;
-                continue;
-            }
-            else{
+            if(nums[r]==nums[l])r++;
+            else if(nums[r]!=nums[l]){
                 nums[++l]=nums[r];
             }
         }
